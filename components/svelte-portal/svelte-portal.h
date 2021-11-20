c52561984a2fc4be16ae4569ecf0bd4d764cb825
DrZlo13 <who.just.the.doctor@gmail.com>
2021-11-21 06:51:44 +1000

    add Svelte site as component

---
+++ components/svelte-portal/svelte-portal.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdint.h>
+
+extern const uint8_t index_html_start[] asm("_binary_index_html_start");
+extern const uint8_t index_html_end[] asm("_binary_index_html_end");
+extern const uint8_t build_bundle_css_start[] asm("_binary_bundle_css_start");
+extern const uint8_t build_bundle_css_end[] asm("_binary_bundle_css_end");
+extern const uint8_t build_bundle_js_start[] asm("_binary_bundle_js_start");
+extern const uint8_t build_bundle_js_end[] asm("_binary_bundle_js_end");
