577fb87212dd5e16d8c03141716e5c6f5d0660f5
DrZlo13 <who.just.the.doctor@gmail.com>
2022-04-08 01:11:09 +1000

    Revert "tcp-uart"
    This reverts commit abbb99a008240345f279ef867fa1f961a5e60b39.

--- main/network-uart.h
+++ main/network-uart.h
@@ -12,16 +12,3 @@
  * Start UART server
  */
 void network_uart_server_init(void);
-
-/**
- * Checks if someone is connected to the UART server
- * @return bool
- */
-bool network_uart_connected(void);
-
-/**
- * Send data
- * @param buffer data
- * @param size data size
- */
-void network_uart_send(uint8_t* buffer, size_t size);
